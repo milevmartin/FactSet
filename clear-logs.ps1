a086938dccaf4d1042825b33c2e0be9164db9e09
milevmartin <52923293+milevmartin@users.noreply.github.com>
2020-04-10 17:21:59 +0300

    Update clear-logs.ps1

--- clear-logs.ps1
+++ clear-logs.ps1
@@ -19,28 +19,27 @@ $FreeSpacePRCNT = ($disk.FreeSpace / $disk.Size)*100 #converting the values to b
 while ($FreeSpacePRCNT -lt 20) {
 
 #Get the oldest log files and Remove the file oldest log file        
-    try {
+	try {
 
-
-			$file = Get-ChildItem C:\datacache | Sort-Object -Property LastWriteTime | select -first 1 #get the oldes file
-			$date = Get-Date # get the date
-				if ($file){
-							Get-ChildItem C:\datacache | Sort-Object -Property LastWriteTime | select -first 1 | Remove-Item # remove the oldes file
-							"$date $file.name is deleted" | Out-File -FilePath C:\logs\removedfiles.log -Append #write the results to the log
-				} else{
-						exit;
-				}		       
+		$file = Get-ChildItem C:\datacache | Sort-Object -Property LastWriteTime | select -first 1 #get the oldes file
+		$date = Get-Date # get the date
+			if ($file){
+				Get-ChildItem C:\datacache | Sort-Object -Property LastWriteTime | select -first 1 | Remove-Item # remove the oldes file
+				"$date $file.name is deleted" | Out-File -FilePath C:\logs\removedfiles.log -Append #write the results to the log
+			} else{
+				
+			exit;
+			}		       
         }
 
-    catch [System.IO.FileNotFoundException],[System.IO.IOException]{
- 
-        "$date $file.name was not fould check the error $_.ScriptStackTrace " | Out-File -FilePath C:\logs\removedfiles.log -Append
+	catch [System.IO.FileNotFoundException],[System.IO.IOException]{
+ 	"$date $file.name was not fould check the error $_.ScriptStackTrace " | Out-File -FilePath C:\logs\removedfiles.log -Append
 	
 	exit;     
         }
-    catch {
+	catch {
         
-        "$date unable to delete the file $file Unknown error: $_.ScriptStackTrace " | Out-File -FilePath C:\logs\removedfiles.log -Append #write an error message to the log
+	"$date unable to delete the file $file Unknown error: $_.ScriptStackTrace " | Out-File -FilePath C:\logs\removedfiles.log -Append #write an error message to the log
         
 	exit;        
         }                       
